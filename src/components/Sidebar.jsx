1ad2bc3d19a8cbd8334a5c4de0a89c9ef9a7dfa6
yago <yagosonicmassaroni@gmail.com>
2024-04-09 21:00:42 -0300

    terminado o side bar e o perfil

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import {PencilLine} from "phosphor-react"
+
 import styles from "./Sidebar.module.css"
 
 export function Sidebar() {
@@ -9,14 +11,15 @@ export function Sidebar() {
         alt=""
       />
       <div className={styles.profile}>
-      <img className={styles.avatar} src="https://github.com/massaroni.png"/>
+         <img className={styles.avatar} src="https://github.com/massaroni.png"/>
 
-        <strong>Yago Belo</strong>
-        <span>Web Developer</span>
+          <strong>Yago Belo</strong>
+          <span>Web Developer</span>
       </div>
 
       <footer>
         <a href="#">
+          <PencilLine size={20}/>
           Editar seu perfil
         </a>
       </footer>
