b6732e1fa5c5d5d056c946baa8530f2527e72450
yago <yagosonicmassaroni@gmail.com>
2024-04-11 22:03:02 -0300

    criação da interface pronta

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import igniteLogo from "../assets/ignite-logo.svg"
 
 export function Header() {
   return (
-    <header>
+    <header className={styles.header}>
       <img src={igniteLogo} alt="Logotipo do ignite" />
     </header>
   )
